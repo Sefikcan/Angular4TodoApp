0f8cefce4d031d93221b68ee6c991a7bf605d0e8
SefikCan <sefikcankanber@gmail.com>
2018-01-15 22:09:58 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Models/Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  ItemList : Todo[] =[];
+  Item:Todo = { Completed:false,ItemName:'' };
+
+  AddItem() {
+    console.log('Added Item:' + this.Item.ItemName);
+    this.ItemList.push({'Completed' : this.Item.Completed, 'ItemName' : this.Item.ItemName});
+    this.Item.ItemName = '';
+    console.log(this.ItemList);
+  }
+
+  todoComplete(todo : Todo) {
+    this.ItemList.find(x => x.ItemName == todo.ItemName).Completed = true;
+    console.log(this.ItemList);
+  }
+
+  delete(todo : Todo) {
+    let index = this.ItemList.indexOf(todo);
+    this.ItemList.splice(index, 1);
+  }
 }
